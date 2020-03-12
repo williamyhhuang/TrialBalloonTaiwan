8edcf19d35badf902a9853e628efe650930f37bb
williamyhhuang <yhhuang1992@gmail.com>
2020-03-12 20:49:25 +0800

    Init mysql set up

--- init_mysql/init.sh
+++ init_mysql/init.sh
@@ -1,5 +1,5 @@
 echo "import tbt data..."
-mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" tbt< /data/tbt_data_20200229.sql
+mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" tbt< /data/tbt_data_20200312.sql
 echo "set mysql_native_password..."
 mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e"ALTER USER 'test'@'%' IDENTIFIED WITH mysql_native_password BY '1234'";
 echo "done"
