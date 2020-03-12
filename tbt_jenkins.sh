c4170c6211e830e887a4d14c2bbdc7010c656ea8
williamyhhuang <yhhuang1992@gmail.com>
2020-03-13 00:30:43 +0800

    Modify jenkins shell script

--- tbt_jenkins.sh
+++ tbt_jenkins.sh
@@ -1,3 +1,5 @@
+echo "jenkins start remove docker image"
+docker rmi $(docker images -f "dangling=true" -q)
 echo "jenkins start docker-compose"
 /usr/local/bin/docker-compose up -d --build
 echo "jenkins docker-compose done"
